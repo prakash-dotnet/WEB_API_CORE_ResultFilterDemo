a2cafaf025147925f3a4a03a58df6f7a92b7f79d
praka <praka@PRAKASHLAPTOP>
2024-12-17 19:48:28 +0530

    Add project files.

---
+++ WEB_API_CORE_ResultFilterDemo/Controllers/SampleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API_CORE_ResultFilterDemo.Controllers
+{
+   // [ServiceFilter(typeof(CustomResultFilter))]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+    
+        public IActionResult Get()
+        {
+            return Ok(new { Id = 1, Name = "Prakash" });
+        }
+    }
+}
---
+++ WEB_API_CORE_ResultFilterDemo/CustomResultFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WEB_API_CORE_ResultFilterDemo
+{
+    public class CustomResultFilter : IResultFilter
+    {
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            // Modify the result before it's executed
+            if (context.Result is ObjectResult objectResult)
+            {
+                // Wrap the response object in a custom structure
+                objectResult.Value = new
+                {
+                    Success = true,
+                    Data = objectResult.Value,
+                    Message = "Operation completed successfully"
+                };
+            }
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            // Code to run after the result has been executed
+        }
+    }
+}
